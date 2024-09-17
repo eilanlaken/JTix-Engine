605dc4b8cae2f895078ae9affe35a926e401fd66
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-09-17 10:07:09 -0700

    m

--- src/main/java/com/heavybox/jtix/assets/AssetLoader.java
+++ src/main/java/com/heavybox/jtix/assets/AssetLoader.java
@@ -8,4 +8,14 @@ public interface AssetLoader<T> {
     void asyncLoad(final String path);
     T create();
 
+    // TODO: finalize asset loading.
+    // tasks: font loading
+    // shader loading
+    // parameters
+    // asset management, free and delete
+    // handle background tasks - texture packing etc together with loading.
+    class Options<T> {
+
+    }
+
 }
