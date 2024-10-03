9a7c0d18bd199c2348b9573702707075fb80c9d8
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-10-03 11:08:36 -0700

    m

--- src/main/java/com/heavybox/jtix/math/Matrix4x4.java
+++ src/main/java/com/heavybox/jtix/math/Matrix4x4.java
@@ -1535,7 +1535,7 @@ public class Matrix4x4 implements MemoryPool.Reset {
 
     @Override
     public String toString() {
-        return "[" + val[M00] + "|" + val[M01] + "|" + val[M02] + "|" + val[M03] + "]\n" //
+        return    "[" + val[M00] + "|" + val[M01] + "|" + val[M02] + "|" + val[M03] + "]\n" //
                 + "[" + val[M10] + "|" + val[M11] + "|" + val[M12] + "|" + val[M13] + "]\n" //
                 + "[" + val[M20] + "|" + val[M21] + "|" + val[M22] + "|" + val[M23] + "]\n" //
                 + "[" + val[M30] + "|" + val[M31] + "|" + val[M32] + "|" + val[M33] + "]\n";
