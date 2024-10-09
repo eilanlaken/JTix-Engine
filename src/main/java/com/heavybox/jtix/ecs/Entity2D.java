088692e5b37352c689549070ecba8139e1c41121
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-10-09 15:26:21 -0700

    m

--- src/main/java/com/heavybox/jtix/ecs/Entity2D.java
+++ src/main/java/com/heavybox/jtix/ecs/Entity2D.java
@@ -1,10 +1,14 @@
 package com.heavybox.jtix.ecs;
 
+import com.heavybox.jtix.collections.Array;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class Entity2D extends Entity {
 
     public final ComponentTransform2D transform;
+
+    private Entity2D parent;
+    private Array<Entity2D> children;
 
     protected Entity2D() {
         this(0,0,0,1,1);
@@ -16,6 +20,26 @@ public abstract class Entity2D extends Entity {
 
     protected Entity2D(float x, float y, float deg, float sclX, float sclY) {
         this.transform = new ComponentTransform2D(x, y, deg, sclX, sclY);
+    }
+
+    // TODO
+    protected void setParent(Entity2D parent, boolean keepTransform) {
+
+    }
+
+    // TODO
+    protected void clearParent(Entity2D parent, boolean keepTransform) {
+
+    }
+
+    protected void attachChild(Entity2D child) {
+        if (child == null) throw new ECSException("Child entity cannot be null");
+        if (child == this) throw new ECSException("Cannot add an " + Entity2D.class.getSimpleName() + " as a child to itself.");
+        if (child.parent != null) throw new ECSException("child " + Entity2D.class.getSimpleName() + " already has a parent. Must un-parent first.");
+        if (this.children != null && this.children.contains(child,true)) throw new ECSException(Entity2D.class.getSimpleName() + " child is already a child of this " + Entity2D.class.getSimpleName());
+        if (this.children == null) this.children = new Array<>(false, 1);
+        this.children.add(child);
+        child.parent = this;
     }
 
     @Override protected          ComponentTransform2D createComponentTransform() { return transform; }
