5cfb5fd40c4eb347376963a33ba1ef836e915204
Eilan Laken <eilanlaken@gmail.com>
2025-03-27 23:49:35 +0200

    preping for major visual improvement

---
+++ src/main/java/com/heavybox/jtix/zzz/ToolTerrainMask.java
@@ -0,0 +1,13 @@
+package com.heavybox.jtix.zzz;
+
+public class ToolTerrainMask {
+
+    public Mode currentMode = Mode.ADD;
+
+    public enum Mode {
+        ADD,
+        SUB,
+        ;
+    }
+
+}
