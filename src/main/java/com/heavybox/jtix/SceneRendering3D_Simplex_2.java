3bcb53af2dae9d16fba0f1c19aade7820c13a470
Eilan Laken <eilanlaken@gmail.com>
2025-04-22 20:41:43 +0300

    m

--- src/main/java/com/heavybox/jtix/SceneRendering3D_Simplex_2.java
+++ src/main/java/com/heavybox/jtix/SceneRendering3D_Simplex_2.java
@@ -5,6 +5,7 @@ import com.heavybox.jtix.assets.Assets;
 import com.heavybox.jtix.graphics.*;
 import com.heavybox.jtix.input.Input;
 import com.heavybox.jtix.input.Keyboard;
+import com.heavybox.jtix.input.Mouse;
 import com.heavybox.jtix.math.Matrix4x4;
 import com.heavybox.jtix.math.Vector3;
 import org.lwjgl.opengl.GL11;
@@ -61,12 +62,29 @@ public class SceneRendering3D_Simplex_2 implements Scene {
         Vector3 screen = new Vector3(Input.mouse.getX(), Input.mouse.getY(), 0);
         camera.unProject(screen);
 
-        camera.update();
-
-        if (Input.keyboard.isKeyPressed(Keyboard.Key.K)) {
-            //world.createConstraintDistance(body_a, body_b, 4);
-            camera.position.z += 0.1f;
+        float scroll = Input.mouse.getVerticalScroll();
+        if (Input.keyboard.isKeyJustPressed(Keyboard.Key.INSERT)) {
+            if (camera.mode == Camera.Mode.ORTHOGRAPHIC) camera.mode = Camera.Mode.PERSPECTIVE;
+            else camera.mode = Camera.Mode.ORTHOGRAPHIC;
         }
+        if (Input.mouse.getVerticalScroll() != 0) {
+            if (camera.mode == Camera.Mode.PERSPECTIVE) camera.translateForward(scroll * 30);
+            else camera.zoom += 0.04f * scroll;
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_SHIFT) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panHorizontal = Input.mouse.getXDelta();
+            float panVertical = Input.mouse.getYDelta();
+            camera.translateRight(-panHorizontal);
+            camera.translateUp(panVertical);
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_CONTROL) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panVertical = Input.mouse.getYDelta();
+            camera.translateForward(-panVertical);
+        } else if (Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panHorizontal = Input.mouse.getXDelta() * 0.2f;
+            float panVertical = Input.mouse.getYDelta() * 0.2f;
+            camera.rotateAroundAxis(panHorizontal * 5,0,0,1);
+            camera.rotateAroundRight(panVertical * 5);
+        }
+        camera.update();
 
 
         if (Input.keyboard.isKeyPressed(Keyboard.Key.DOWN)) {
--- src/main/java/com/heavybox/jtix/SceneRendering3D_Simplex_3.java
+++ src/main/java/com/heavybox/jtix/SceneRendering3D_Simplex_3.java
@@ -63,22 +63,24 @@ public class SceneRendering3D_Simplex_3 implements Scene {
         camera.unProject(screen);
 
 
-        float scroll = 3 * Input.mouse.getVerticalScroll();
+        float scroll = Input.mouse.getVerticalScroll();
         if (Input.mouse.getVerticalScroll() != 0) {
-            camera.translateForward(scroll);
-        }
-        if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_SHIFT) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
-            float panHorizontal = Input.mouse.getXDelta() * 0.2f;
-            float panVertical = Input.mouse.getYDelta() * 0.2f;
+            if (camera.mode == Camera.Mode.PERSPECTIVE) camera.translateForward(scroll * 30);
+            else camera.zoom += 0.04f * scroll;
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_SHIFT) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panHorizontal = Input.mouse.getXDelta();
+            float panVertical = Input.mouse.getYDelta();
             camera.translateRight(-panHorizontal);
             camera.translateUp(panVertical);
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_CONTROL) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panVertical = Input.mouse.getYDelta();
+            camera.translateForward(-panVertical);
         } else if (Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
             float panHorizontal = Input.mouse.getXDelta() * 0.2f;
             float panVertical = Input.mouse.getYDelta() * 0.2f;
-            camera.rotateAroundUp(panHorizontal * 5);
+            camera.rotateAroundAxis(panHorizontal * 5,0,0,1);
             camera.rotateAroundRight(panVertical * 5);
         }
-
         camera.update();
 
 
