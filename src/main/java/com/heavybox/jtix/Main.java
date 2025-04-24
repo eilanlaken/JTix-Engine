d6266c85cb6693f6ae077fdd396b5ed743e19aa9
Eilan Laken <eilanlaken@gmail.com>
2025-04-24 19:30:15 +0300

    imported some more assets, now fixing z value

--- src/main/java/com/heavybox/jtix/Main.java
+++ src/main/java/com/heavybox/jtix/Main.java
@@ -3,6 +3,7 @@ package com.heavybox.jtix;
 import com.heavybox.jtix.application.Application;
 import com.heavybox.jtix.application.ApplicationSettings;
 import com.heavybox.jtix.zzz_planes.ScenePlanesGame_TileBuilder_3;
+import com.heavybox.jtix.zzz_planes.ScenePlanesGame_TileBuilder_4;
 
 import java.io.IOException;
 
@@ -65,7 +66,7 @@ public class Main {
         ApplicationSettings settings = new ApplicationSettings();
         settings.decorated = true;
         Application.init(settings); // can init with options.
-        Application.launch(new SceneRendering3D_Shadows_1());
+        Application.launch(new ScenePlanesGame_TileBuilder_4());
 
     }
 
---
+++ src/main/java/com/heavybox/jtix/SceneRendering3D_ModelsImport_2.java
@@ -0,0 +1,159 @@
+package com.heavybox.jtix;
+
+import com.heavybox.jtix.application.Scene;
+import com.heavybox.jtix.assets.Assets;
+import com.heavybox.jtix.graphics.*;
+import com.heavybox.jtix.input.Input;
+import com.heavybox.jtix.input.Keyboard;
+import com.heavybox.jtix.input.Mouse;
+import com.heavybox.jtix.math.Matrix4x4;
+import com.heavybox.jtix.math.Vector3;
+import org.lwjgl.opengl.GL11;
+
+// contact points polygon vs polygon:
+// https://www.youtube.com/watch?v=5gDC1GU3Ivg
+public class SceneRendering3D_ModelsImport_2 implements Scene {
+
+    private Camera camera;
+
+    public Model model_blue_box;
+    public Model model_floor;
+    public Matrix4x4 transform_blue_box = new Matrix4x4();
+    public Matrix4x4 transform_floor = new Matrix4x4();
+    Renderer2D renderer2D = new Renderer2D();
+
+    public SceneRendering3D_ModelsImport_2() {
+
+    }
+
+    @Override
+    public void setup() {
+
+        Assets.loadModel("assets/models/floor.fbx");
+        Assets.loadModel("assets/app-models/vehicle-truck_4.fbx", "assets/app-models/textures");
+        Assets.finishLoading();
+
+        model_floor = Assets.get("assets/models/floor.fbx");
+        model_blue_box = Assets.get("assets/app-models/vehicle-truck_4.fbx");
+
+    }
+
+    @Override
+    public void finish() {
+
+    }
+
+    @Override
+    public void start() {
+        camera = new Camera(Camera.Mode.PERSPECTIVE, Graphics.getWindowWidth(), Graphics.getWindowHeight(), 1, 1, 100, 75);
+        camera.position.set(0, -7, 5);
+
+        camera.lookAt(0,0,0);
+
+        camera.update();
+
+        transform_blue_box.translateGlobalAxisXYZ(0,0,3);
+
+    }
+
+
+    @Override
+    public void update() {
+        Vector3 screen = new Vector3(Input.mouse.getX(), Input.mouse.getY(), 0);
+        camera.unProject(screen);
+
+        float scroll = Input.mouse.getVerticalScroll();
+        if (Input.keyboard.isKeyJustPressed(Keyboard.Key.INSERT)) {
+            if (camera.mode == Camera.Mode.ORTHOGRAPHIC) camera.mode = Camera.Mode.PERSPECTIVE;
+            else camera.mode = Camera.Mode.ORTHOGRAPHIC;
+        }
+        if (Input.mouse.getVerticalScroll() != 0) {
+            if (camera.mode == Camera.Mode.PERSPECTIVE) camera.translateForward(scroll * 10);
+            else camera.zoom += 0.04f * scroll;
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_SHIFT) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panHorizontal = Input.mouse.getXDelta();
+            float panVertical = Input.mouse.getYDelta();
+            camera.translateRight(-panHorizontal);
+            camera.translateUp(panVertical);
+        } else if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT_CONTROL) && Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panVertical = Input.mouse.getYDelta();
+            camera.translateForward(-panVertical);
+        } else if (Input.mouse.isButtonPressed(Mouse.Button.MIDDLE)) {
+            float panHorizontal = Input.mouse.getXDelta() * 0.2f;
+            float panVertical = Input.mouse.getYDelta() * 0.2f;
+            camera.rotateAroundAxis(panHorizontal * 2,0,0,1);
+            camera.rotateAroundRight(panVertical * 2);
+        }
+        camera.update();
+
+
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.DOWN)) {
+            transform_blue_box.translateGlobalAxisXYZ(0,0,-0.05f);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.UP)) {
+            transform_blue_box.translateGlobalAxisXYZ(0,0,0.05f);
+        }
+
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.LEFT)) {
+            transform_blue_box.translateGlobalAxisXYZ(0,-0.05f, 0);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.RIGHT)) {
+            transform_blue_box.translateGlobalAxisXYZ(0,0.05f,0);
+        }
+
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.E)) {
+            transform_blue_box.rotateGlobalAxisY(1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.Q)) {
+            transform_blue_box.rotateGlobalAxisY(-1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.W)) {
+            transform_blue_box.rotateGlobalAxisZ(1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.S)) {
+            transform_blue_box.rotateGlobalAxisZ(-1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.A)) {
+            transform_blue_box.rotateGlobalAxisX(1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.D)) {
+            transform_blue_box.rotateGlobalAxisX(-1);
+        }
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.H)) {
+            transform_blue_box.translateGlobalAxisXYZ(0,1,0);
+        }
+
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.R)) {
+            //body_a.applyForce(1,0, body_a.shape.x(), body_a.shape.y() + 0.2f);
+        }
+
+        if (Input.keyboard.isKeyJustPressed(Keyboard.Key.SPACE)) {
+            //world.createConstraintWeld(body_a, body_b, new Vector2(1,0));
+        }
+
+        if (Input.keyboard.isKeyPressed(Keyboard.Key.F)) {
+            Renderer3D.lightDir.rotate(1f,1,0,0);
+            System.out.println(Renderer3D.lightDir);
+        }
+
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glClearColor(0.247f,0.247f,0.247f,1);
+
+        renderer2D.begin();
+        //renderer2D.drawCircleFilled(300, 30,0,0,0,1,1);
+        renderer2D.end();
+
+        Renderer3D.begin(camera);
+        for (int i = 0; i < model_blue_box.meshes.length; i++) {
+            Renderer3D.drawModel_tmp_5(model_blue_box.meshes[i], model_blue_box.materials[i], transform_blue_box);
+        }
+
+        for (int i = 0; i < model_floor.meshes.length; i++) {
+            Renderer3D.drawModel_tmp_5(model_floor.meshes[i], model_floor.materials[i], transform_floor);
+        }
+        Renderer3D.end();
+    }
+
+
+
+}
