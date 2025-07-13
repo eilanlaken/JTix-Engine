321079b0260dfa526b802c09a70dfb895d3b5e0f
Eilan Laken <eilanlaken@gmail.com>
2025-07-13 10:01:35 +0300

    m

--- src/main/java/com/heavybox/jtix/Main.java
+++ src/main/java/com/heavybox/jtix/Main.java
@@ -73,6 +73,7 @@ public class Main {
         ApplicationSettings settings = new ApplicationSettings();
         Application.init(settings); // can init with options.
         Application.launch(new SceneRendering3D_Airplanes_1());
+        //Application.launch(new SceneRendering3D_ModelsViewer_Map_1());
         //Application.launch(new ScenePlanesGame_Terrain_New_9());
         //Application.launch(new SceneRendering3D_VFX_1());
         //Application.launch(new SceneRendering3D_NewClouds());
